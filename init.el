7533b5431c5771a8eb73d189439b175b86ad5614
azfa <binsu00@hotmail.com>
2019-07-30 15:01:30 +0800

    created package el in lisp folder

--- init.el
+++ init.el
@@ -1,52 +1,5 @@
-(when (>= emacs-major-version 24)
-     (require 'package)
-     (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
-
-;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
-
- ;; cl - Common Lisp Extension
- (require 'cl)
-
- ;; Add Packages
- (defvar my/packages '(
-		;; --- Auto-completion ---
-		company
-		;; --- Better Editor ---
-		hungry-delete
-;;		smex
-		swiper
-		counsel
-		smartparens
-		;; --- Major Mode ---
-		js2-mode
-		;; --- Minor Mode ---
-		nodejs-repl
-		exec-path-from-shell
-		;; --- Themes ---
-		monokai-theme
-		;; solarized-theme
-		) "Default packages")
-
- (setq package-selected-packages my/packages)
-
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
-
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
-
- ;; Find Executable Path on OS X
-;; (when (memq window-system '(mac ns))
-;; (exec-path-from-shell-initialize))
-
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require  'init-packages)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 ;;(electric-indent-mode 1)
@@ -88,9 +41,9 @@
  ;; If there is more than one, they won't work right.
  )
 
-(global-company-mode t)
 (setq-default cursor-type 'bar)
 (setq make-backup-files nil)
+(setq auto-save-default nil)
 
 ;;输入 <s 再按TAB键
 (require 'org)
@@ -105,16 +58,12 @@
 ;;(setq initial-frame-alist (quote ((fullscreen . maximized))))
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (global-hl-line-mode t)
-(load-theme 'monokai t)
-(global-hungry-delete-mode)
 
 ;;(require 'smex) ; Not needed if you use package.el
 ;;  (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
 ;;					; when Smex is auto-initialized on its first run.
 ;;(global-set-key (kbd "M-x") 'smex)
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
@@ -125,14 +74,6 @@
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
 (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(require 'smartparens-config)
-(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-;;(add-hook 'org-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
 ;; example of setting env var named “path”, by appending a new path to existing path
 (setenv "env"
   (concat
@@ -157,3 +98,5 @@
    (t
     (shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" default-directory) "\"")))))
 (global-set-key (kbd "C-x C-a") 'locate-current-file-in-explorer)
+(global-auto-revert-mode t)
+(setq ring-bell-function 'ignore)
